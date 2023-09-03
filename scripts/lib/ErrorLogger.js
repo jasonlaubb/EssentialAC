2a7baa67f27dcffeccb1248ca5a8a88529b82094
jasonlaubb <136591629+MeatBallGG@users.noreply.github.com>
2023-09-03 14:49:03 +0800

    Update ErrorLogger.js

--- scripts/lib/ErrorLogger.js
+++ scripts/lib/ErrorLogger.js
@@ -1,6 +1,5 @@
 import { world } from '@minecraft/server';
-import config from '../default/config.js';
 export function errorlogger(e, modules){
-  if(config.debug) world.sendMessage(`§9Essential §l§7>§r§g catch §cerror §gin §c${modules}§r§g at ${e.printStackTrace()} as §4${e.name}: §c${e.message}`);
-  console.warn(`Essential > catch error in ${modules} as ${e.name}: ${e.message}`)
+  world.sendMessage(`§9Essential §l§7>§r§g catch §cerror §gwith §c${e.name}: ${e.message} ${e.stack}`);
+  console.warn(`Essential > ERROR: ${e.name}: ${e.message} ${e.stack}`)
 };
