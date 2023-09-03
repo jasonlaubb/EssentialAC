0f2a461e4df0aeb8469368da1973ee7bafaa3a0e
jasonlaubb <136591629+MeatBallGG@users.noreply.github.com>
2023-09-03 13:55:06 +0800

    Update DataBase.js

--- scripts/lib/DataBase.js
+++ scripts/lib/DataBase.js
@@ -1,14 +1,20 @@
 import { world } from '@minecraft/server';
+import { errorlogger } from './ErrorLogger.js';
 
 export function create(name, value) {
+try {
   if (world.scoreboard.getObjective(`database:${name}`) == undefined) {
     world.scoreboard.addObjective(`database:${name}`, 'database');
     world.getDimension('minecraft:overworld').runCommand(`scoreboard players set ${JSON.stringify(JSON.stringify(value))} database:${name} 2147483647`);
     return console.log(`database: New database created - ${name}`);
   } else return console.warn(`database: Failed to create DataBase - ${name}`)
+} catch (e) {
+  errorlogger(e, "DataBase/create")
+}
 };
 
 export function get(name) {
+try {
   if (world.scoreboard.getObjective(`database:${name}`) !== undefined) {
     const output = JSON.parse(JSON.parse(`"${world.scoreboard.getObjective(`database:${name}`).getParticipants()[0].displayName}"`));
     return output
@@ -16,19 +22,30 @@ export function get(name) {
     console.warn(`database: Failed to get DataBase - ${name}`);
     return undefined
   }
+} catch (e) {
+  errorlogger(e, "DataBase/get")
+}
 };
 
 export function state(name) {
+try {
   if (world.scoreboard.getObjective(`database:${name}`) == undefined) {
     return false
   } else return true
+} catch (e) {
+  errorlogger(e, "DataBase/state")
+}
 };
 
 export function reload(name, value) {
+try {
   if (world.scoreboard.getObjective(`database:${name}`) !== undefined) {
     world.databasechanged = true;
     world.scoreboard.removeObjective(`database:${name}`);
     world.scoreboard.addObjective(`database:${name}`, 'database');
     return world.getDimension('minecraft:overworld').runCommand(`scoreboard players set ${JSON.stringify(JSON.stringify(value))} database:${name} 2147483647`);
   } else return console.warn(`database: Failed to reload DataBase - ${name}`)
+} catch (e) {
+  errorlogger(e, "DataBase/reload")
+}
 };
