2d75de359da90f98caa2da3f7515911b176e7807
jasonlaubb <136591629+MeatBallGG@users.noreply.github.com>
2023-09-03 13:56:42 +0800

    Update Language.js

--- scripts/lib/Language.js
+++ scripts/lib/Language.js
@@ -1,11 +1,16 @@
 import en from '../lang/en_US.js';
 import ch from '../lang/ch_TW.js';
 import jp from '../lang/JPN.js';
+import { errorlogger } from './ErrorLogger.js';
 
 function langs(language) {
+try {
   if(language == 0) return en
   if(language == 1) return ch
   if(language == 2) return jp
+} catch (e) {
+  errorlogger(e, "language")
+}
 };
 
 export { langs }
